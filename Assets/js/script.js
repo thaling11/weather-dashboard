6e0e161c1577162f749ff9d2670dd8e63c950753
thaling <thaling@verizon.net>
2022-07-26 17:32:11 -0600

    JS pseudocode

---
+++ Assets/js/script.js
@@ -0,0 +1,16 @@
+//weather-api cUrl: website{id}
+//ID is available to us, based on data-id on click for the #get-weather button
+
+//Data Flow: 
+//button click event listener
+//use id to fetch api weather data
+//update website text content with weather api data
+
+
+//variables
+const searchButton = document.querySelector("#search-button");
+//functions
+
+//spectial function --event listeners
+
+//logic/start app if required
