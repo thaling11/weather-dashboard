746a8d2f244a7bc509cec15aa86c83d870a9208a
thaling <thaling@verizon.net>
2022-07-27 13:15:33 -0600

    sewtting up JS weather conditions

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,12 +1,11 @@
 //weather-api cUrl: website{id}
-var APIkey = "e3c9bafef9a3ce15a82181805234f419";
+var APIkey = "a680d80e94900005ef907a18f28f496e";
 //ID is available to us, based on data-id on click for the #get-weather button
 
-//Data Flow: 
+//Data Flow:
 //button click event listener
 //use id to fetch api weather data
 //update website text content with weather api data
-
 
 //variables
 const searchButton = $("#search-button");
@@ -18,25 +17,76 @@ var currentCity;
 
 //open weather API to get weather from coordinates
 function retrieveWeather(data) {
-    let requestUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${data.lat}&lon=${data.lon}&exclude=minutely,hourly,alerts&units=metric&appid=${APIkey}`
-    fetch(requestUrl)
-    .then(function(response) {
-        return response.json();
+  let requestUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${data.lat}&lon=${data.lon}&exclude=minutely,hourly,alerts&units=metric&appid=${APIkey}`;
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
     })
-    .then(function(data) {
-        var currentWeather = $("#currentWeather");
-        currentWeather.addClass("border border-primary");
+    //current weather
+    .then(function (data) {
+      var currentWeather = $("#currentWeather");
+      currentWeather.addClass("border border-primary");
+      //city name element
+      var cityName = $("<h2>");
+      cityName.text(currentCity);
+      currentWeather.append(cityName);
+      //date appended to city name
+      var currentCityDate = data.current.dt;
+      currentCityDate = moment.unix(currentCityDate).format("MM/DD/YYYY");
+      var currentDate = $("<span>");
+      currentDate.text(`(${currentCityDate})`);
+      cityName.append(currentWeatherIcon);
+      //weather icon append to city name
+      var currentCityWeatherIcon = data.current.weather[0].icon;
+      var currentWeatherIcon = $("<img>");
+      currentWeatherIcon.attr(
+        "src",
+        "http://openweathermap.org/img/wn/" + currentCityWeatherIcon + ".png"
+      );
+      cityName.append(currentWeatherIcon);
+      //current temp data
+      var currentTemp = data.current.temp;
+      var currentTempElement = $("<p>");
+      currentTemp.text(`Temp: ${currentTemp}°C`);
+      currentWeather.append(currentTempElement);
 
-        var cityName = $("<h2>");
-        cityName.text(currentCity);
-        currentWeather.append(cityName);
+      //current wind speed
+      var currentWind = data.current.wind_speed;
+      var currentWindElement = $("<p>");
+      currentWindElement.text(`Wind: ${currentWind} KPH`);
+      currentWeather.append(currentWindElement);
 
-        var currentCityDate = data.current.dt;
-        currentCityDate = moment.unix(currentCityDate).format("MM/DD/YYYY");
-        var currentDate = $("<span>");
-        currentDate.text(`(${currentCityDate})`);
-        cityName.append(currentWeatherIcon);
-    })
+      //current humidity
+      var currentHumidity = data.current.humidity;
+      var currentHumidityElement = $("<p>");
+      currentHumidityElement.text(`Humidity: ${currentHumidity}%`);
+      currentWeather.append(currentHumidityElement);
+      
+      //current UV index 
+      var currentUV = data.current.uvi;
+      var currentUvElement = $('<p>');
+      var currentUvSpanEl = $('<span>');
+      currentUvElement.append(currentUvSpanEl);
+      currentUvSpanEl.text(`UV: ${currentUV}`)
+      
+      //set background color based on level 
+    if ( currentUV < 3 ) {
+        currentUvSpanEl.css({'background-color':'green', 'color':'white'});
+    } else if ( currentUV < 6 ) {
+        currentUvSpanEl.css({'background-color':'yellow', 'color':'black'});
+    } else if ( currentUV < 8 ) {
+        currentUvSpanEl.css({'background-color':'orange', 'color':'white'});
+    } else if ( currentUV < 11 ) {
+        currentUvSpanEl.css({'background-color':'red', 'color':'white'});
+    } else {
+        currentUvSpanEl.css({'background-color':'violet', 'color':'white'});
+    }
+
+    currentWeather.append(currentUvElement);
+
+    //5 day forecast
+
+    });
 }
 
 //spectial function --event listeners
