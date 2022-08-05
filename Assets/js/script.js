98c4b967498654cd550eb5540de22310fb0830f0
thaling <thaling@verizon.net>
2022-08-05 11:01:24 -0600

    event listener

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,12 +1,11 @@
 //variables
 
 var APIkey = "a680d80e94900005ef907a18f28f496e";
-
+var searchBtn = document.querySelector("#search-button");
 
 //fetch API weather data
 function getWeatherdata() {
-    var requestUrl = `https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=${a680d80e94900005ef907a18f28f496e}`
-
+    var requestUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${a680d80e94900005ef907a18f28f496e}`
     fetch(requestUrl)
     .then(function (response) {
       return response.json();
@@ -19,6 +18,10 @@ function getWeatherdata() {
         return;
 };
 
+//store in local storage
+// function storeData(data) {
+// }
+
 //five day forecast
 
 
@@ -27,5 +30,8 @@ function getWeatherdata() {
 //add history
 
 //event listener to search button/clear button
-
+searchBtn.addEventListener("click", function(event) {
+    cityName = document.querySelector("#city-input").value 
+    getWeatherdata();
+})
 
